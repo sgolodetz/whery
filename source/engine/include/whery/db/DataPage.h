d408d19b33b189eb43d0fe8225a78c3188103376
sgolodetz <sgolodetz@gxstudios.net>
2013-05-22 00:18:53 +0100

    Start work on DataPage.

---
+++ source/engine/include/whery/db/DataPage.h
@@ -0,0 +1,21 @@
+/**
+ * whery: DataPage.h
+ * Copyright Stuart Golodetz, 2013. All rights reserved.
+ */
+
+#ifndef H_WHERY_DATAPAGE
+#define H_WHERY_DATAPAGE
+
+#include <map>
+
+namespace whery {
+
+class DataPage
+{
+private:
+	// TODO
+};
+
+}
+
+#endif
